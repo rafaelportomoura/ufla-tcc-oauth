8a7e31ca9471a937a86540fe95116370ab538954
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-23 20:12:02 -0300

    fix: 🐛 stack name

--- deploy/stacks/api.py
+++ deploy/stacks/api.py
@@ -3,7 +3,7 @@ from scripts.stacks import Stack, stack_name
 
 
 def my_stack_name(stage: str, tenant: str) -> str:
-    return stack_name(stage=stage, tenant=tenant, name="OAuth-Ecr")
+    return stack_name(stage=stage, tenant=tenant, name="OAuth-Api")
 
 
 def stack(
