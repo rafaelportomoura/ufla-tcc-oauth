8300158cd5a4bf2d38ff5222f4760f672f3366be
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-04-23 01:32:38 -0300

    feat(stacks): deploy

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -1,6 +1,9 @@
 from scripts.typescript import Typescript
 from scripts.cloudformation import CloudFormation
 from scripts.args import get_args
+from stacks import ecs, ecr, api, cognito
+from scripts.exception import DeployException
+from scripts.docker import Docker
 
 args = get_args(
     {
@@ -10,6 +13,17 @@ args = get_args(
         "region": {"type": "str", "required": False, "default": "us-east-2"},
         "profile": {"type": "str", "required": False, "default": "default"},
         "log_level": {"type": "int", "required": False, "default": 3},
+        "account_id": {"type": "str", "required": True},
+        "min_container": {"type": "int", "required": False, "default": 1},
+        "max_container": {"type": "int", "required": True, "default": 1},
+        "scale_out_cooldown": {"type": "int", "required": False, "default": 60},
+        "scale_in_cooldown": {"type": "int", "required": False, "default": 60},
+        "cpu_utilization": {"type": "int", "required": False, "default": 40},
+        "authorizer_result_ttl_in_seconds": {
+            "type": "int",
+            "required": False,
+            "default": 300,
+        },
     }
 )
 
@@ -19,4 +33,72 @@ tenant = args["tenant"]
 region = args["region"]
 profile = args["profile"]
 log_level = args["log_level"]
+account_id = args["account_id"]
 
+cloudformation = CloudFormation(profile=profile, region=region, log_level=log_level)
+
+################################################
+# 🚀 COGNITO
+################################################
+COGNITO_STACK = cognito.stack(stage=stage, tenant=tenant)
+cloudformation.deploy_stack(stack=COGNITO_STACK)
+
+if not cloudformation.stack_is_succesfully_deployed(stack_name=COGNITO_STACK["stack_name"]):
+    raise DeployException(stack=COGNITO_STACK)
+
+################################################
+# 🚀 ECR
+################################################
+ECR_STACK = ecr.stack(stage=stage, tenant=tenant, microservice=microservice)
+cloudformation.deploy_stack(stack=ECR_STACK)
+if cloudformation.stack_is_succesfully_deployed(stack_name=ECR_STACK["stack_name"]):
+    ecr_uri = Docker.ecr_uri(account_id=account_id, region=region)
+    docker = Docker(ecr_uri=ecr_uri, image=microservice, tag=stage)
+    typescript = Typescript()
+    typescript.build()
+    docker.build()
+    docker.push()
+else:
+    raise DeployException(stack=ECR_STACK)
+
+################################################
+# 🚀 API_GATEWAY
+################################################
+exports = cloudformation.list_exports()
+listener_arn = cloudformation.get_export_value(exports=exports, name=f"{stage}-{tenant}-application-load-balancer-listener-arn")
+
+API_GATEWAY_STACK = api.stack(
+    stage=stage,
+    tenant=tenant,
+    microservice=microservice,
+    base_path="oauth",
+    listener_arn=listener_arn,
+    authorizer_result_ttl_in_seconds=args["authorizer_result_ttl_in_seconds"],
+    log_level=log_level,
+)
+
+if not cloudformation.stack_is_succesfully_deployed(stack_name=API_GATEWAY_STACK["stack_name"]):
+    raise DeployException(stack=API_GATEWAY_STACK)
+
+################################################
+# 🚀 ECS
+################################################
+exports = cloudformation.list_exports()
+key_arn = cloudformation.get_export_value(exports=exports, name="encrypt-key-arn")
+target = cloudformation.get_export_value(
+    exports=exports, name=f"{stage}-{tenant}-{microservice}-target-group-arn"
+)
+
+ECS_STACK = ecs.stack(
+    stage=stage,
+    tenant=tenant,
+    microservice=microservice,
+    log_level=log_level,
+    key_arn=key_arn,
+    min_container=args["min_container"],
+    max_container=args["max_container"],
+    scale_out_cooldown=args["scale_out_cooldown"],
+    scale_in_cooldown=args["scale_in_cooldown"],
+    cpu_utilization=args["cpu_utilization"],
+    target_group=target,
+)
