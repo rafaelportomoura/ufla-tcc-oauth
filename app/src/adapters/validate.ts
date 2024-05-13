8f4b6aad2ce050f78613542bee45ca2c2b3b7a41
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-13 17:52:49 -0300

    test: 💍 exceptions/*

--- app/src/adapters/validate.ts
+++ app/src/adapters/validate.ts
@@ -10,6 +10,6 @@ export class Validator<T> {
 
     if (result.success) return result.data;
 
-    throw new ValidationError(result.error.format());
+    throw new ValidationError(result.error.formErrors.fieldErrors);
   }
 }
