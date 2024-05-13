5ebf7e8ca3e53dbd1c69616a3451dfdb75f404aa
rafaelportomoura <rafael.moura.dev.29@gmail.com>
2024-05-13 16:53:13 -0300

    test: controller/createCustomer

---
+++ app/tests/unit/controllers/createCustomer.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import Sinon from 'sinon';
+import { CreateUserBusiness } from '../../../src/business/createUser';
+import { CODE_MESSAGES } from '../../../src/constants/codeMessages';
+import { createCustomer } from '../../../src/controllers/createCustomer';
+import { ValidationError } from '../../../src/exceptions/ValidationError';
+import { fastify_reply, fastify_request } from '../../data/fastify';
+import { UsersData } from '../../data/users';
+
+describe('Controller -> CreateCustomer', async () => {
+  beforeEach(Sinon.restore);
+  const res = fastify_reply();
+  it('Should create customer', async () => {
+    const status_spy = Sinon.spy(res, 'status');
+    const req = fastify_request({ body: UsersData.create_user() });
+    Sinon.stub(CreateUserBusiness.prototype, 'create').resolves();
+    const response = await createCustomer(req, res);
+    expect(status_spy.args).deep.eq([[201]]);
+    expect(response).to.deep.equal(CODE_MESSAGES.CUSTOMER_CREATED);
+  });
+  it('Should create customer', async () => {
+    const status_spy = Sinon.spy(res, 'status');
+    const req = fastify_request({ body: UsersData.create_user({ username: 2 as unknown as string }) });
+    const response = await createCustomer(req, res);
+    expect(status_spy.args).deep.eq([[400]]);
+    expect(response).instanceOf(ValidationError);
+  });
+});
